93e73ca8800d03f7d9f8147382cea48df74e2114
yyx <1390939057@qq.com>
2021-01-15 15:45:38 +0800

    feat: 添加Constants:Permission、Time

---
+++ app/src/main/java/com/yyx/common/constants/TimeConstants.java
@@ -0,0 +1,13 @@
+package com.yyx.common.constants;
+
+
+
+public final class TimeConstants {
+
+    public static final int MSEC = 1;
+    public static final int SEC  = 1000;
+    public static final int MIN  = 60000;
+    public static final int HOUR = 3600000;
+    public static final int DAY  = 86400000;
+
+}
